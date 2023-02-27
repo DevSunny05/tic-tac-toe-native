3805335442c5f494abae7ec9d82fce200b0ba6d9
sarang <sarang.office01@gmail.com>
2023-02-27 19:13:32 +0530

    initialize project

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground/>
+      <Text>Hello Sarang</Text>
       <StatusBar style="auto" />
     </View>
   );
